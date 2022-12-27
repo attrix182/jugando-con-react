c413828e92152930e84544ff7473f861d27364f5
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2022-12-27 18:35:12 -0300

    Feat creando vista de formulario

--- src/components/Navbar.component.tsx
+++ src/components/Navbar.component.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Jugando con React
+            Playing with React
           </Typography>
        
         </Toolbar>
