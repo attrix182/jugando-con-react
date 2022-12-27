8f2895c5f9ec18a2f14eefdd91682b5b1d8ea36a
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2022-12-27 18:22:25 -0300

    Instalando MUI

---
+++ src/pages/home.component.tsx
@@ -0,0 +1,13 @@
+import CreateUserForm from '../components/CreateUser-form.component';
+import Navbar from '../components/Navbar.component';
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <CreateUserForm />
+    </div>
+  );
+}
+
+export default Home;
